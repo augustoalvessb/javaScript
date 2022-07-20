6e8160bd971457e2c2387a5d940688083f3e2ccf
augustoalvessb <augustohenriqueptc@hotmail.com>
2022-07-20 17:37:48 -0300

    Adiocina Exercícios

---
+++ curso-em-video/js07/ex08.js
@@ -0,0 +1,6 @@
+var vel = 44
+console.log(`Sua velocidade é ${vel}Km/h`)
+if (vel > 40) {
+    console.log('Você ultrapassou o limite de segurança. MULTADO!')
+}
+console.log('Dirija sempre com segurança!')
