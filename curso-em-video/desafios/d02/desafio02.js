937fda62a2dbec459241c233abee9ab056946f14
augustoalvessb <augustohenriqueptc@hotmail.com>
2022-08-27 11:43:27 -0300

    atualizando desafio

--- curso-em-video/desafios/d02/desafio02.js
+++ curso-em-video/desafios/d02/desafio02.js
@@ -1,5 +1,5 @@
 function inicio() {
-    var nome = prompt('Qual é seu nome?')
-    var idade = prompt(`Olá, ${nome}!! Quantos anos você tem?`)
-    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} de idade!`)
+    var nome = prompt('Qual é seu nome?');
+    var idade = prompt(`Olá, ${nome}!! Quantos anos você tem?`);
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} de idade!`);
 }
--- curso-em-video/desafios/d04/troco.js
+++ curso-em-video/desafios/d04/troco.js
@@ -1,8 +1,16 @@
 function calcular() {
     var produto = prompt('Que produto você está comprando?');
-    var custo = parseFloat(prompt(`Quanto custa ${produto} que você esta comprando?`));
-    var pagamento = parseFloat(prompt(`Qual foi o valor que você deu para pagar ${produto}?`))
-    var troco = pagamento - custo; 
 
-    window.alert(`Você comprou ${produto} que custou R$${custo.toLocateString('pt-BR', {style:'currency', currency: 'BRL'})}.\nDeu R$${pagamento.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})} em dinheiro e vai receber R$${troco.toLocateString('pt-BR', {style:'currency', currency: 'BRL'})} de troco.\nObrigado e volte sempre!`);
+    var quantidade = parseInt(prompt(`Qual quantidade de ${produto} você está comprando?`));
+
+    var custo = Number(prompt(`Quanto custa ${produto} que você esta comprando?`));
+
+    var total = custo * quantidade;
+
+    var pagamento = Number(prompt(`Total a pagar será ${total.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})}. \nQual o valor que você dará para pagar ${produto}?`));
+
+
+    var troco = pagamento - custo * quantidade; 
+
+    window.alert(`Você comprou ${quantidade} ${produto} que custou ${custo.toLocaleString('pt-BR', {style:'currency', currency: 'BRL'})} cada.\nDeu ${pagamento.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})} em dinheiro e vai receber ${troco.toLocaleString('pt-BR', {style:'currency', currency: 'BRL'})} de troco.\nObrigado e volte sempre!`);
 }
