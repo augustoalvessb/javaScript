668d0b9092b3ddaa0c21be054ee70cb3f98ca670
augustoalvessb <augustohenriqueptc@hotmail.com>
2023-01-12 12:24:37 -0300

    add projetos da máquina local

---
+++ quiz/js/questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        question: "Quanto é 1 + 2?",
+        answers: [
+            { option: "2", correct: false },
+            { option: "4", correct: false },
+            { option: "3", correct: true },
+        ],
+    },
+    {
+        question: "Quanto é 5 x 7?",
+        answers: [
+            { option: "35", correct: true },
+            { option: "38", correct: false },
+            { option: "21", correct: false },
+        ],
+    },
+    {
+        question: "Quanto é 81 / 9?",
+        answers: [
+            { option: "7", correct: false },
+            { option: "8", correct: false },
+            { option: "9", correct: true },
+        ],
+    },
+    {
+        question: "Quanto é 88 - 35?",
+        answers: [
+            { option: "50", correct: false },
+            { option: "53", correct: true },
+            { option: "45", correct: false },
+        ],
+    },
+];
